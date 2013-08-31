2279c5f0b238f9f4b36cfad14ea11959d89e147c
Tyson324 <sgonez@hotmail.com>
2013-08-31 18:18:51 +0100

    Moved files from Old Repo

---
+++ css/axurerp_pagespecificscript.js
@@ -0,0 +1,24 @@
+﻿for(var i = 0; i < 63; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u51'] = 'center';document.getElementById('u60_img').tabIndex = 0;
+
+u60.style.cursor = 'pointer';
+$axure.eventManager.click('u60', function(e) {
+
+if (true) {
+
+	ScrollToWidget('u62', false,true,'swing',500);
+function waitu83e51f6191bb4a4d8084af46a51bd79a1() {
+
+	MoveWidgetTo('u11', GetNum('0'), GetNum('1270'),'swing',500);
+}
+setTimeout(waitu83e51f6191bb4a4d8084af46a51bd79a1, 1000);
+
+}
+});
+gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u61'] = 'center';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u23'] = 'center';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u49'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u21'] = 'center';gv_vAlignTable['u53'] = 'center';gv_vAlignTable['u33'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u55'] = 'center';
+
